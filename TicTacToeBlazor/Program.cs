0980468d7d37f799da25bb9a81d8db7352919d82
Michael Bond <mbond@ltisolutions.com>
2022-03-02 18:11:45 -0500

    Add and use head outlet, remove HttpClient

--- TicTacToeBlazor/Program.cs
+++ TicTacToeBlazor/Program.cs
@@ -1,9 +1,9 @@
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TicTacToeBlazor;
 
-WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
